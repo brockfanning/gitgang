e08a750b210f1dca4f057bfa20126771cd170eaa
Brock Fanning <brockfanning@gmail.com>
2019-08-26 18:24:58 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='gitgang',
-      version='0.0.3',
+      version='0.0.4',
       description='Reject pull-requests that are not from members of your gang.',
       url='https://github.com/brockfanning/gitgang',
       author='Brock Fanning',
